a9a16acfc8c290d5cec4d1f77836214d305d60f2
Aayush <thakur29aayush@gmail.com>
2026-06-21 18:03:58 +0545

    Basic Setup of Backend Complete

---
+++ src/config/prisma.js
@@ -0,0 +1,10 @@
+const { PrismaClient } = require("../generated/prisma/client");
+
+const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "info", "warn", "error"]
+      : ["warn", "error"],
+});
+
+module.exports = prisma;
