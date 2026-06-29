c2823185f0dab2a3124335b1b759b6c4378a5686
Aayush20253534 <aayush.20253534@mnnit.ac.in>
2026-06-29 22:43:05 +0545

    final fix-0

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -21,4 +21,22 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message = error.response?.data?.message || "";
+
+    if (
+      error.response?.status === 401 &&
+      /authentication token missing|authentication required|invalid or expired token/i.test(
+        message
+      )
+    ) {
+      error.response.data.message = "Login session expired. Please login again.";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;
