4917a2769f029df5a0b99b3b4c660ea4d9412eb0
Aayush20253534 <aayush.20253534@mnnit.ac.in>
2026-06-29 14:38:43 +0545

    sidebar change

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -24,7 +24,7 @@ export default function DashboardLayout({ items, title }) {
   return (
     <div className="flex min-h-screen bg-bg-soft">
       <aside
-        className={`fixed inset-y-0 left-0 z-40 flex w-72 flex-col border-r border-line bg-white transition-transform lg:static lg:translate-x-0 ${
+        className={`fixed inset-y-0 left-0 z-40 flex h-screen w-72 shrink-0 flex-col border-r border-line bg-white transition-transform lg:sticky lg:top-0 lg:translate-x-0 ${
           open ? "translate-x-0" : "-translate-x-full"
         }`}
       >
